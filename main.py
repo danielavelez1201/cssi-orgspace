09aae72d767a3e3746ef91ef3ad571b2fd8f8019
Samuel Sandoval <sjsandoval1000@gmail.com>
2019-07-31 16:56:03 -0700

    Changes to mainFeed, searchresults

--- main.py
+++ main.py
@@ -21,41 +21,6 @@ class Profile(ndb.Model):
     bio = ndb.StringProperty(required = False)
     usertype = ndb.StringProperty(required =True)
 
-class UpdateProfile(webapp2.RequestHandler):
-    def get(self):
-        user = users.get_current_user().email()
-        profile = Profile.query().filter(user == Profile.email).get()
-        template_vars={
-            "profile" : profile
-        }
-        template = jinja_env.get_template('templates/updateProfile.html')
-        self.response.write(template.render(template_vars))
-
-    def post(self):
-        location = self.request.get("location")
-        category = self.request.get("category")
-        bio = self.request.get("bio")
-        phone = self.request.get("phone")
-        fullname = self.request.get("fullname")
-
-        user = users.get_current_user().email()
-        profile = Profile.query().filter(user == Profile.email).get()
-
-        profile.location = self.request.get("location")
-        profile.category = self.request.get("category")
-        profile.bio = self.request.get("bio")
-        profile.phone = int(self.request.get("phone"))
-
-        profile.put()
-
-        self.redirect("/organizationProfilePage")
-        # user = Profile.query().filter
-
-class signupprofile (webapp2.RequestHandler):
-     def get(self):
-         mainFeed_template = jinja_env.get_template('templates/signupprofile.html')
-         self.response.write(mainFeed_template.render())  # the response
-
 class Event(ndb.Model):
     organization = ndb.KeyProperty(kind = Profile)
     title = ndb.StringProperty(required = True)
@@ -111,6 +76,36 @@ class Update(webapp2.RequestHandler):
         self.redirect('/organizationProfilePage')
         user = Profile.query().filter
 
+class updateProfile(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user().email()
+        profile = Profile.query().filter(user == Profile.email).get()
+        template_vars={
+            "profile" : profile
+        }
+        template = jinja_env.get_template('templates/updateProfile.html')
+        self.response.write(template.render(template_vars))
+
+    def post(self):
+        location = self.request.get("location")
+        category = self.request.get("category")
+        bio = self.request.get("bio")
+        phone = self.request.get("phone")
+        fullname = self.request.get("fullname")
+
+        user = users.get_current_user().email()
+        profile = Profile.query().filter(user == Profile.email).get()
+
+        profile.location = self.request.get("location")
+        profile.category = self.request.get("category")
+        profile.bio = self.request.get("bio")
+        profile.phone = int(self.request.get("phone"))
+
+        profile.put()
+
+        self.redirect("/organizationProfilePage")
+        # user = Profile.query().filter
+
 class signupprofile (webapp2.RequestHandler):
      def get(self):
          mainFeed_template = jinja_env.get_template('templates/signupprofile.html')
@@ -119,21 +114,37 @@ class signupprofile (webapp2.RequestHandler):
 class searchresults(webapp2.RequestHandler):
     def get(self):
         search_query = self.request.get('search_query')
-        profiles = Profile.query().filter(search_query == Profile.fullname).fetch()
+        profiles = Profile.query().filter(Profile.fullname == search_query).fetch()
         template_vars = {
             'profiles' : profiles,
+            'search_query' : search_query,
         }
         template = jinja_env.get_template('templates/searchresults.html')
         self.response.write(template.render(template_vars))
 
-class viewprofile(webapp2.RequestHandler):
-    def get(self):
-        profile=ndb.Key(urlsafe=self.request.get("userkey")).get()
-        template_vars = {
-        'profile' : profile,
-        }
-        template = jinja_env.get_template('templates/searchresults.html')
-        self.response.write(template.render(template_vars))
+# class (webapp2.RequestHandler):
+#     def get(self):
+#         profile = self.request.get("profile")
+#         profileKey=ndb.Key(urlsafe= profile)
+#         profile = profileKey.get()
+#
+#         template_vars = {
+#         'profile' : profile,
+#         }
+#         # user = users.get_current_user().email()
+#         profileKey = self.request.get("profile")
+#         profiles = Profile.query().filter(profile == Profile.ProfilePage).get()
+#         # logging.info(user)
+#         template = jinja_env.get_template('templates/searchresults.html')
+#         self.response.write(template.render(template_vars))
+#
+#         # event = self.request.get("event")
+#         # eventKey = ndb.Key(urlsafe=event)
+#         # event = eventKey.get()
+#         # template_vars = {
+#         #     'event' : event
+        # }
+
 
 
 class MainHandler(webapp2.RequestHandler):
@@ -144,7 +155,6 @@ class MainHandler(webapp2.RequestHandler):
         orguser = Profile(
             fullname=self.request.get('fullname'),
             email=user.email(),
-
             location=self.request.get('location'),
             category=self.request.get('category'),
             phone= int(self.request.get('phone')),
@@ -448,11 +458,10 @@ app = webapp2.WSGIApplication([
 ('/donatePost', donatePost),
 ('/createPost', createPost),
 ('/signupprofile', signupprofile),
-('/updateProfile', UpdateProfile),
+('/updateProfile', updateProfile),
 ('/thankyouPost', thankyouPost),
 ('/about', About),
 ('/searchresults', searchresults),
-('/viewprofile', viewprofile),
 ('/collaborators', collaborators),
 
 # ('/organizationProfilePage', organizationProfilePage),
